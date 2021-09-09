89fdfee6185d18cd8cf4591f9c77315564fc0600
Rafael <rafaelcacilhas@gmail.com>
2021-09-09 17:11:07 -0300

    insta

---
+++ semana-5/insta4/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import Post from './components/Post/Post';
+
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'rafa'}
+          fotoUsuario={'https://picsum.photos/50/50?a=2'}
+          fotoPost={'https://picsum.photos/200/150?a=2'}
+        />
+
+        <Post
+          nomeUsuario={'lucas'}
+          fotoUsuario={'https://picsum.photos/50/50?a=3'}
+          fotoPost={'https://picsum.photos/200/150?a=3'}
+        />
+
+
+
+
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
---
+++ semana-5/react-intro/react-experimentos/blackjack/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './comp/Card';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Card imagem="" valor='4' naipe="paus"/>
+
+    </div>
+  );
+}
+
+export default App;
